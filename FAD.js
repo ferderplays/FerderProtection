cf52fde3e0fbf5659e6e861f4bea0204768b503e
FERDdeveloper <86313430+FERDdeveloper@users.noreply.github.com>
2021-09-19 14:39:51 +0200

    Anti-DDOS
    DDOS protection for websites. Only works for coded websites!(wich is obvious, but i mention it anyway)

---
+++ FAD.js
@@ -0,0 +1,9 @@
+//AntiDDOS protection, coded with clever ddesing, and good knowledge about Java/JavaScript
+//Meaning: if system registers DDOS attack it does the things inside the "{}"
+if(System.registeredDDOS()) {
+    do {
+        //stops DDOS attack in 0.7 seconds, however this can be changed to lower time
+        System.stopDDOS( .inTime(0.7));
+    }
+}
+//Important: This only works for websites(or discord bots)
