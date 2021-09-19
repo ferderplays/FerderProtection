9f36bbf2b261ee60c25dc9d12ebe1d0be00ef078
FERDdeveloper <86313430+FERDdeveloper@users.noreply.github.com>
2021-09-19 14:59:04 +0200

    Concept of Anti-virus
    This is only concept, it's not actually working well.

---
+++ FAV.java
@@ -0,0 +1,15 @@
+import javax.swing.text.html.parser.Entity;
+
+public class A {
+    int b = Entity(width = "20px", height = "20px", color = "Green", border = 2px solid red,type="button");if(b.pressed=True)
+    {
+        do {
+            System.scanFiles( in(System.fileDirectories()));
+            if(System.foundMalware()) {
+                do {
+                    this.displayTitle("Malware found!");
+                }
+            }
+        }
+    }
+}
