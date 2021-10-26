6f4eaae5cb44f5c8ec686a1bfda248cad8c676bc
FERDdeveloper <86313430+FERDdeveloper@users.noreply.github.com>
2021-10-26 15:41:31 +0200

    anti-virus 1.0.0
    public class A {
        int b = Entity(width = "20px", height = "20px", color = "Green", border = 2px solid red,type="button");if(b.pressed=True)
        {
            do {
                System.scanFiles( in(System.fileDirectories()));
                if(System.foundMalware()) {
                    do {
                        this.displayTitle("Malware found!"); **how did i thought this will work?**

--- FAV.java
+++ FAV.java
@@ -1,13 +1,32 @@
 import javax.swing.text.html.parser.Entity;
 
-public class A {
-    int b = Entity(width = "20px", height = "20px", color = "Green", border = 2px solid red,type="button");if(b.pressed=True)
-    {
-        do {
-            System.scanFiles( in(System.fileDirectories()));
-            if(System.foundMalware()) {
-                do {
-                    this.displayTitle("Malware found!");
+public class FAV extends JavaTE {
+    void noFunction() {
+        var1 = malw;
+        String malw = 'System.getProperty("") + ""';
+    }
+    static objects() {
+        file_selector = new object(func =>(this.selectFile));
+        scanner = new object(func => (thsi.checkCode().ofFile(file_selector.selectedFile())));
+    }
+    void scanning() {
+        if (file_selector.selectedFile() = true) {
+            scanner.seeCode().from(file_selector);
+        }
+    }
+    void scanned() {}
+    for (va2 = 0) {
+        var2 = 0;
+        var3 = var1;
+        var4 = scanner;
+
+        if (scanner.found(var3)) {
+            do {
+                String hulk this.displayTitle("malware found");
+                while (hulk = true) {
+                    do {
+                        this.displayCode();
+                    }
                 }
             }
         }
